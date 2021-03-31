fcc4bc3a416bac6e0f7da89850c344df8a4349d3
Andrew Kihs <ajkihs@gmail.com>
2021-03-31 17:15:30 -0400

    Include unique username in Users table

--- first_routes/app/models/user.rb
+++ first_routes/app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
 
-    validates :name, presence: true
-    validates :email, presence: true
-
+   
+    validates: uniqueness: false
     
 end
 
---
+++ first_routes/db/migrate/20210331210915_correct_users_unique.rb
@@ -0,0 +1,5 @@
+class CorrectUsersUnique < ActiveRecord::Migration[5.2]
+  def change
+    change_column :users, :username, :string, unique: true, null: false
+  end
+end
