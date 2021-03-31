a84bf65c4bcfd7e48f4acd5f35369731cedae5e3
Travis <tputnam8170@gmail.com>
2021-03-31 17:00:17 -0400

    Create update,destroy,show

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -13,10 +13,37 @@ class UsersController < ApplicationController
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+
+        unless user.nil?
+            render json: user
+        else
+            render json: user.errors.full_messages, status: 404
+        end
     end
     
     # def user_params
     #     params.require(:user).permit(:name, :email)
     # end
+
+    def destroy
+         user = User.find(params[:id])
+
+        unless user.nil?
+            user.destroy
+        else
+            render json: user.errors.full_messages, status: 404
+        end
+    end
+
+    def update
+        user = User.find(params[:id])
+
+        # unless user.nil?
+        if user.update_attributes(params.require(:user).permit(:name, :email))
+            render json: user
+        else
+            render json: user.errors.full_messages, status: 404
+        end
+    end
 end
