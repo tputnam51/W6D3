513fd4b209814b8bb597e0b62c90fe1297db1ad9
Andrew Kihs <ajkihs@gmail.com>
2021-03-31 16:18:13 -0400

    Build create method with error handling

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -3,11 +3,20 @@ class UsersController < ApplicationController
         render json: User.all
     end
 
-    def create
-        render json: params
+    def create      
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
         render json: params
     end
+    
+    # def user_params
+    #     params.require(:user).permit(:name, :email)
+    # end
 end
